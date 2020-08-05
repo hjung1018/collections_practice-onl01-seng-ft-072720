608c6ea710d48eed94af717f43156184c7fbbe69
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-05 01:17:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,8 +6,15 @@ def sort_array_desc_array(array)
   array.sort!
 end
 
-def sort_array_char_count
-  
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length > b.length
+      1
+    elsif a.length < b.length
+    -1
+  elsif a.length == b.length
+  0
+end
 end
 
 def swap_elements
